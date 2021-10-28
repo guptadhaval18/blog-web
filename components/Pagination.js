0a270ffd3a047c4e189f08cbd83bb14c4e088a7f
guptadhaval18 <guptadhaval18@gmail.com>
2021-10-28 21:58:47 +0530

    Path aliases

--- components/Pagination.js
+++ components/Pagination.js
@@ -12,14 +12,14 @@ export default function Pagination({ currentPage, numPages }) {
     <div className='mt-6'>
       <ul className='flex pl-0 list-none my-2'>
         {!isFirst && (
-          <Link href={prevPage}>
+          <Link key='prev' href={prevPage}>
             <li className='relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-gray-800 mr-1 hover:bg-gray-200 cursor-pointer'>
               Previous
             </li>
           </Link>
         )}
         {Array.from({ length: numPages }, (_, i) => (
-          <Link href={`/blog/page/${i + 1}`}>
+          <Link key={i + 1} href={`/blog/page/${i + 1}`}>
             <li className='relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-gray-800 mr-1 hover:bg-gray-200 cursor-pointer'>
               {i + 1}
             </li>
@@ -27,7 +27,7 @@ export default function Pagination({ currentPage, numPages }) {
         ))}
 
         {!isLast && (
-          <Link href={nextPage}>
+          <Link key='nxt' href={nextPage}>
             <li className='relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-gray-800 mr-1 hover:bg-gray-200 cursor-pointer'>
               Next
             </li>
